27cc353828bea97401134a0c058b34f22d06f502
Jinse <wlstp8473@khu.ac.kr>
2026-03-06 20:21:14 +0900

    sbb_2_11

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -5,17 +5,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/question")
 public class QuestionController {
 
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     //@ResponseBody -> 템플릿 사용으로 인해, 필요없어짐
     public String list(Model model){
         List<Question> questionList = this.questionService.getList();
@@ -23,7 +25,7 @@ public class QuestionController {
         return "question_list"; //템플릿 파일 반환
     }
 
-    @GetMapping(value = "/question/detail/{id}")
+    @GetMapping(value = "/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id){
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
