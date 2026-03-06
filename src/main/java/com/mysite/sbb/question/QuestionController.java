3d3cba86da144f8bcdcbe0e633bc793969604e96
Jinse <wlstp8473@khu.ac.kr>
2026-03-06 19:45:58 +0900

    sbb_02_09

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -12,12 +12,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class QuestionController {
 
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
     @GetMapping("/question/list")
     //@ResponseBody -> 템플릿 사용으로 인해, 필요없어짐
     public String list(Model model){
-        List<Question> questionList = this.questionRepository.findAll();
+        List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list"; //템플릿 파일 반환
     }
